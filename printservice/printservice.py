40e0db32a8b69cc1c89aedf35f931ff81ed980b2
Björn Pedersen <bjoern.pedersen@frm2.tum.de>
2017-11-23 15:32:22 +0100

    Add a simple example print service

---
+++ printservice/printservice.py
@@ -0,0 +1,31 @@
+#/usr/bin/env python
+
+from flask import Flask
+from flask import request
+from werkzeug.utils import secure_filename
+import socket
+#
+config = {'printer': 'localhost'}
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, Printer Proxy!'
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(secure_filename(f.filename))
+        sendToPrinter(f)
+    return 'success'
+
+def sendToPrinter(stream):
+    try:
+        printer = socket.socket()
+        printer.connect((config['printer'], 9100))
+        printer.send(stream)
+        printer.close()
+    except socket.error:
+        return False
